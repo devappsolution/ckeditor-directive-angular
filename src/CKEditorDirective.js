ef75e395cf196be245c20a14230c3d6c5572179c
Paulo Roberto de Souza <paulo.prsdesouza@gmail.com>
2017-03-30 17:10:15 -0300

    first commit

---
+++ src/CKEditorDirective.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    angular.module("app").directive("appCkeditor", [
+        CKEditorDirective
+    ]);
+
+    function CKEditorDirective() {
+        return {
+            restrict: "A",
+            scope: {
+                conteudo: "=appCkeditor"
+            },
+            link: function ($scope, $element, attrs) {
+                var config = {
+                    filebrowserBrowseUrl: "/Browse",
+                    filebrowserUploadUrl: "/Upload-Thumbnail",
+                    allowedContent: true
+                }
+
+                var editor = CKEDITOR.replace($element[0], config);
+
+                editor.on('change', updateModel);
+                editor.on('dataReady', setData);
+
+                function setData() {
+                    if ($scope.conteudo) {
+                        editor.setData($scope.conteudo);
+                    }
+                }
+
+                function updateModel() {
+                    $scope.$apply(function () {
+                        $scope.conteudo = editor.getData();
+                    });
+                }
+            }
+        }
+    }
+})();
